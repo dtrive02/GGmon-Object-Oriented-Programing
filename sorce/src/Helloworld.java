a28af8b5f5ecbd5877ed9884a168ee3856165fdb
deepak <deepakvktrivedi@gmail.com>
2026-02-02 17:32:27 -0500

    jvavaaa

--- sorce/src/Helloworld.java
+++ sorce/src/Helloworld.java
@@ -1,6 +1,6 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("helloWWorld");
+        System.out.println("Lab  changes to HelloWorld Program");
         return;
     }
 }
